2d87ca11a01036123d620d18c2b921b6d218006f
Pablo10544 <pablo10544@hotmail.com>
2025-09-15 17:48:48 -0300

    Objeto wallets, com pegar todas as wallets e pegar por chave

---
+++ backend/wallets.py
@@ -0,0 +1,14 @@
+class Wallets:
+    def __init__(self):        
+     self.wallets = []
+     self.wallets.append({
+    "chave": "GACGQKLIML7NGOSFXAKS6UXSG4ZOFBNOYFDG7VYZYZGSZ266GUHG74JT",
+    "pass": "SAVQ66BWHZIEGF5TOHZX3QBX62QT4UDAM6GW3O34EAGX4NMQFXIROA3Z"
+})
+    def get_all_wallets(self):
+        return self.wallets
+    def get_wallet_by_key(self, chave):
+        for wallet in self.wallets:
+            if wallet["chave"] == chave:
+                return wallet
+        return None
